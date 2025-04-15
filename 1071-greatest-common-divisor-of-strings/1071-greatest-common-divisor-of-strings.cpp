7a0afec1e1ab448603589b67b3d82cff0de8fcbd
hyzoon <shj83811081@gmail.com>
2025-04-16 01:09:45 +0900

    Time: 3 ms (22.09%), Space: 12.3 MB (12.75%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,11 +1,46 @@
 class Solution {
 public:
-    string gcdOfStrings(string str1, string str2) {
+    // 문자열을 n번 반복하는 함수
+    string repeat(const string& s, int times) {
+        string result;
+        for (int i = 0; i < times; ++i) {
+            result += s;
+        }
+        return result;
+    }
 
-        if (str1 + str2 != str2 + str1) {
-            return "";
+    string gcdOfStrings(string str1, string str2) {
+        
+        int gcdLen = gcd(str1.length(), str2.length());
+        string sub1, sub2;
+        string gcdStr = "";
+
+        for (int i = 1; i <= gcdLen ; i++) {
+            
+            if (str1.length() % i != 0 || str1.length() % i != 0) {
+                continue;
+            }
+            
+            sub1 = str1.substr(0,i);
+            sub2 = str2.substr(0,i);
+
+            if (sub1 != sub2) {
+                break;
+            }
+
+            if (repeat(sub1,str1.length()/i) == str1 && repeat(sub2,str2.length()/i) == str2) {
+                gcdStr = str1.substr(0,i);
+            }
+
+            else {
+                if (str1[i] != str2[i]) {
+                    break;
+                }
+            }
+
         }
 
-        return str1.substr(0, gcd(str1.size(), str2.size()));
+        return gcdStr;
+        
     }
 };
