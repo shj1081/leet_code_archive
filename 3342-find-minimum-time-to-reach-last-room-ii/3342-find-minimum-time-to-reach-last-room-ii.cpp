e8b93d2ae34f3934f148e7ad71463e34462e68ee
hyzoon <shj83811081@gmail.com>
2025-05-08 16:42:40 +0900

    Time: 214 ms (85.65%), Space: 97.1 MB (82.49%) - LeetHub

---
+++ 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size();
+        int m = moveTime[0].size();
+
+        vector<vector<int>> minTime(n, vector<int>(m, INT_MAX));
+        using T = tuple<int, int, int, int>; // time, x, y, turn
+        priority_queue<T, vector<T>, greater<T>> pq;
+        pq.push({0, 0, 0, 0});
+        minTime[0][0] = 0;
+
+        int dx[4] = {-1,1,0,0};
+        int dy[4] = {0,0,-1,1};
+
+        while (!pq.empty()) {
+            auto [t, x, y, turn] = pq.top(); pq.pop();
+            if (x == n - 1 && y == m - 1) return t;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i], ny = y + dy[i];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+                int wait = max(t, moveTime[nx][ny]);
+                int nextT = wait + (turn % 2 + 1);
+
+                if (nextT < minTime[nx][ny]) {
+                    minTime[nx][ny] = nextT;
+                    pq.push({nextT, nx, ny, turn + 1});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
