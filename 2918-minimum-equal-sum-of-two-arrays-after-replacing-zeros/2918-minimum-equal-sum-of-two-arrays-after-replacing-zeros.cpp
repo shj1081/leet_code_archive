5dc6f075c6fb44a6b49fc60153104ee2705207a5
hyzoon <shj83811081@gmail.com>
2025-05-10 20:54:23 +0900

    Time: 130 ms (59.66%), Space: 135.5 MB (50.17%) - LeetHub

--- 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    long long minSum(vector<int>& nums1, vector<int>& nums2) {        
-        long long nums1_sum = 0, nums2_sum = 0;
-        long long nums1_zeroes = 0, nums2_zeroes = 0;
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
 
-        for (int x : nums1) {
-            if (x == 0) {
-                nums1_zeroes++;
-                nums1_sum++; // Replace 0 with 1
-            } else {
-                nums1_sum += x;
+        long long sum1=0, sum2=0;
+        bool zero_flag1=false, zero_flag2=false;
+
+        for(auto &n :nums1){
+            sum1 += n;
+
+            if(n==0) {
+                sum1++;
+                zero_flag1 = true;
             }
         }
 
-        for (int x : nums2) {
-            if (x == 0) {
-                nums2_zeroes++;
-                nums2_sum++; // Replace 0 with 1
-            } else {
-                nums2_sum += x;
+        for(auto &n :nums2){
+            sum2 += n;
+
+            if(n==0) {
+                sum2++;
+                zero_flag2 = true;
             }
         }
 
-        if ((nums1_zeroes == 0 && nums2_sum > nums1_sum) ||
-            (nums2_zeroes == 0 && nums1_sum > nums2_sum))
+        if(zero_flag1 == false && sum1 < sum2 || zero_flag2 == false && sum2 < sum1){
             return -1;
+        } 
 
-        return max(nums1_sum, nums2_sum);
+        return max(sum1,sum2);
+
     }
 };
