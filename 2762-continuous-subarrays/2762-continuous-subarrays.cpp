544b04648c53f1e0df1bd712cd6a6ae05afe23f1
hyzoon <shj83811081@gmail.com>
2025-05-12 22:17:06 +0900

    Time: 92 ms (68.53%), Space: 113 MB (75.76%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.cpp
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    static long long continuousSubarrays(vector<int>& nums) {
+    long long continuousSubarrays(vector<int>& nums) {
         const int n = nums.size();
         long long cnt = 0;
-        multiset<int> window;
+        map<int, int> freq;
         int l = 0;
 
-        for (int r = 0; r < n; r++) {
-            window.insert(nums[r]);
+        for (int r = 0; r < n; ++r) {
+            freq[nums[r]]++;  // 오른쪽 확장
 
-            while (*window.rbegin() - *window.begin() > 2) {
-                window.erase(window.find(nums[l]));
+            // 최댓값 - 최솟값 > 2 라면 왼쪽 줄이기
+            while (prev(freq.end())->first - freq.begin()->first > 2) {
+                freq[nums[l]]--;
+                if (freq[nums[l]] == 0) {
+                    freq.erase(nums[l]);  // 개수 0이면 완전히 삭제
+                }
                 l++;
             }
 
-            cnt += r - l + 1;
+            cnt += r - l + 1;  // 현재 구간에서 가장 오늘쪽을 포함하는 가능한 연속 sub vec 수 누적
         }
 
         return cnt;
