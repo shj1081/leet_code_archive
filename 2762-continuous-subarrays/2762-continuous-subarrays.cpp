398a333d12439d1b300cbebf869f62089400e8ad
hyzoon <shj83811081@gmail.com>
2025-05-12 22:15:20 +0900

    Time: 239 ms (12.12%), Space: 156.1 MB (16.9%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.cpp
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-    long long continuousSubarrays(vector<int>& nums) {
-        
-        // runtime
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
-
-
-        
-        // 최대 최소를 빠르게 찾기 위해 자동으로 정렬이 가능한 multiset 사용
+    static long long continuousSubarrays(vector<int>& nums) {
+        const int n = nums.size();
+        long long cnt = 0;
         multiset<int> window;
-        int left = 0;
-        long long res = 0;
+        int l = 0;
 
-        // 윈도우 사용 -> 투포인터를 통해 시간 복잡도 줄이기
-        for (int right = 0; right < nums.size(); right++) {
-            window.insert(nums[right]);
+        for (int r = 0; r < n; r++) {
+            window.insert(nums[r]);
 
-            // 조건 위반 시 left를 이동하여 
             while (*window.rbegin() - *window.begin() > 2) {
-                window.erase(window.find(nums[left]));
-                left++;
+                window.erase(window.find(nums[l]));
+                l++;
             }
 
-            // 현재 윈도우에서 조건을 만족하는 right를 포함하는 subarray 조합 수 더해주기
-            res += right - left + 1;
+            cnt += r - l + 1;
         }
 
-        return res;
+        return cnt;
     }
 };
